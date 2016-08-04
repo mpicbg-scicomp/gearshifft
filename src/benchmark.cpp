f8a77e5f06484d8290d4dbc55e42e0a5f230bac2
steinbac <steinbac@mpi-cbg.de>
2016-08-04 19:17:21 +0200

    converted to manual registration of boost.utf test suite, gearshifft::Options throws which needs further attention

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,3 +1,6 @@
+#define BOOST_TEST_NO_MAIN
+#define BOOST_TEST_ALTERNATIVE_INIT_API
+
 #include "benchmark_suite.hpp"
 #include "application.hpp"
 #include "options.hpp"
@@ -41,13 +44,44 @@ gearshifft::Run<Context, FFT_Is_Normalized, FFTs, Precisions> instance;
 using AppT = gearshifft::FixtureApplication<Context>;
 BOOST_GLOBAL_FIXTURE(AppT);
 
-/// called by Boost UTF
-boost::unit_test::test_suite* init_unit_test_suite( int argc, char* argv[] )
+// /// called by Boost UTF
+// boost::unit_test::test_suite* init_unit_test_suite( int argc, char* argv[] )
+// {
+//   // process extra command line arguments for gearshifft
+//   //  if error or help is shown then do not run any benchmarks
+//   if( gearshifft::Options::getInstance().process(argc, argv) == 0 )
+//     return instance();
+//   else
+//     return nullptr;
+// }
+
+namespace utf = boost::unit_test;
+
+bool init_function()
 {
-  // process extra command line arguments for gearshifft
-  //  if error or help is shown then do not run any benchmarks
-  if( gearshifft::Options::getInstance().process(argc, argv) == 0 )
-    return instance();
-  else
-    return nullptr;
+
+  utf::framework::master_test_suite().add( instance() );
+
+    // do your own initialization here
+    // if it successful return true
+
+    // But, you CAN'T use testing tools here
+
+    return true;
+}
+
+//____________________________________________________________________________//
+
+int main( int argc, char* argv[] )
+{
+  //Options::process may not throw!
+  if( gearshifft::Options::getInstance().process(argc, argv) == 0 ){
+
+    //TODO: remove processed items here
+    
+    return ::boost::unit_test::unit_test_main( &init_function, argc, argv );
+  }
+  else {
+    return 1;
+  }
 }
