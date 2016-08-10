18e4ba9c33b47ba65c341648f7f2a751b40fe265
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2016-08-10 12:41:20 +0200

    Better error handling. Checks available GPU memory.

--- inc/cufft.hpp
+++ inc/cufft.hpp
@@ -26,18 +26,18 @@ namespace CuFFT {
 
       struct FFTExecuteForward{
         void operator()(cufftHandle plan, RealType* in, ComplexType* out){
-          CHECK_CUFFT(cufftExecR2C(plan, in, out));
+          CHECK_CUDA(cufftExecR2C(plan, in, out));
         }
         void operator()(cufftHandle plan, ComplexType* in, ComplexType* out){
-          CHECK_CUFFT(cufftExecC2C(plan, in, out, CUFFT_FORWARD));
+          CHECK_CUDA(cufftExecC2C(plan, in, out, CUFFT_FORWARD));
         }
       };
       struct FFTExecuteBackward{
         void operator()(cufftHandle plan, ComplexType* in, RealType* out){
-          CHECK_CUFFT(cufftExecC2R(plan, in, out));
+          CHECK_CUDA(cufftExecC2R(plan, in, out));
         }
         void operator()(cufftHandle plan, ComplexType* in, ComplexType* out){
-          CHECK_CUFFT(cufftExecC2C(plan, in, out, CUFFT_INVERSE));
+          CHECK_CUDA(cufftExecC2C(plan, in, out, CUFFT_INVERSE));
         }
       };
     };
@@ -53,18 +53,18 @@ namespace CuFFT {
 
       struct FFTExecuteForward{
         void operator()(cufftHandle plan, RealType* in, ComplexType* out){
-          CHECK_CUFFT(cufftExecD2Z(plan, in, out));
+          CHECK_CUDA(cufftExecD2Z(plan, in, out));
         }
         void operator()(cufftHandle plan, ComplexType* in, ComplexType* out){
-          CHECK_CUFFT(cufftExecZ2Z(plan, in, out, CUFFT_FORWARD));
+          CHECK_CUDA(cufftExecZ2Z(plan, in, out, CUFFT_FORWARD));
         }
       };
       struct FFTExecuteBackward{
         void operator()(cufftHandle plan, ComplexType* in, RealType* out){
-          CHECK_CUFFT(cufftExecZ2D(plan, in, out));
+          CHECK_CUDA(cufftExecZ2D(plan, in, out));
         }
         void operator()(cufftHandle plan, ComplexType* in, ComplexType* out){
-          CHECK_CUFFT(cufftExecZ2Z(plan, in, out, CUFFT_INVERSE));
+          CHECK_CUDA(cufftExecZ2Z(plan, in, out, CUFFT_INVERSE));
         }
       };
     };
@@ -100,34 +100,64 @@ namespace CuFFT {
    * (CUFFT_R2C, ...) and depending on number of dimensions {1,2,3}.
    */
   template<cufftType FFTType, size_t NDim>
-  size_t estimateAllocSize(const std::array<size_t,NDim>& e, cufftHandle& plan)
+  size_t estimateAllocSize(cufftHandle& plan, const std::array<size_t,NDim>& e)
   {
     size_t s=0;
+    CHECK_CUDA( cufftCreate(&plan) );
     if(NDim==1){
-      CHECK_CUFFT( cufftGetSize1d(plan, e[0], FFTType, 1, &s) );
+      CHECK_CUDA( cufftGetSize1d(plan, e[0], FFTType, 1, &s) );
     }
     if(NDim==2){
-      CHECK_CUFFT( cufftGetSize2d(plan, e[0], e[1], FFTType, &s) );
+      CHECK_CUDA( cufftGetSize2d(plan, e[0], e[1], FFTType, &s) );
     }
     if(NDim==3){
-      CHECK_CUFFT( cufftGetSize3d(plan, e[0], e[1], e[2], FFTType, &s) );
+      CHECK_CUDA( cufftGetSize3d(plan, e[0], e[1], e[2], FFTType, &s) );
     }
     return s;
   }
+
+  template<cufftType FFTType, size_t NDim>
+  size_t estimateAllocSize64(cufftHandle& plan, const std::array<size_t,NDim>& e) {
+    size_t worksize=0;
+    std::array<long long int, 3> lengths = {{0}};
+    std::copy(e.begin(), e.end(), lengths.begin());
+    CHECK_CUDA( cufftCreate(&plan) );
+    CHECK_CUDA( cufftGetSizeMany64(plan, NDim, lengths.data(), NULL, 0, 0, NULL, 0, 0,
+                                    FFTType,
+                                    1, //batches
+                                    &worksize));
+    return worksize;
+  }
+
   /**
    * Plan Creator depending on FFT transform type (CUFFT_R2C, ...).
    */
   template<cufftType FFTType>
   void makePlan(cufftHandle& plan, const std::array<size_t,3>& e){
-    CHECK_CUFFT(cufftPlan3d(&plan, e[0], e[1], e[2], FFTType));
+    CHECK_CUDA(cufftPlan3d(&plan, e[0], e[1], e[2], FFTType));
   }
   template<cufftType FFTType>
   void makePlan(cufftHandle& plan, const std::array<size_t,1>& e){
-    CHECK_CUFFT(cufftPlan1d(&plan, e[0], FFTType, 1));
+    CHECK_CUDA(cufftPlan1d(&plan, e[0], FFTType, 1));
   }
   template<cufftType FFTType>
   void makePlan(cufftHandle& plan, const std::array<size_t,2>& e){
-    CHECK_CUFFT(cufftPlan2d(&plan, e[0], e[1], FFTType));
+    CHECK_CUDA(cufftPlan2d(&plan, e[0], e[1], FFTType));
+  }
+
+  template<cufftType FFTType, size_t NDim>
+  void makePlan64(cufftHandle& plan, const std::array<size_t,NDim>& e) {
+    size_t worksize=0;
+    std::array<long long int, 3> lengths = {{0}};
+    std::copy(e.begin(), e.end(), lengths.begin());
+    CHECK_CUDA( cufftCreate(&plan) );
+    CHECK_CUDA( cufftMakePlanMany64(plan,
+                                     NDim,
+                                     lengths.data(),
+                                     NULL, 0, 0, NULL, 0, 0, //ignored
+                                     FFTType,
+                                     1, //batches
+                                     &worksize));
   }
 
 
@@ -141,8 +171,7 @@ namespace CuFFT {
            typename TPrecision, // double, float
            size_t   NDim // 1..3
            >
-  struct CuFFTImpl
-  {
+  struct CuFFTImpl {
     using Extent = std::array<size_t,NDim>;
     using Types  = typename traits::Types<TPrecision>;
     using ComplexType = typename Types::ComplexType;
@@ -172,18 +201,23 @@ namespace CuFFT {
     ComplexType*       data_transform_ = nullptr; // intermediate buffer
     size_t             data_size_;
     size_t             data_transform_size_;
+    bool               use64bit_       = false;
 
     CuFFTImpl(const Extent& cextents) {
       extents_ = interpret_as::column_major(cextents);
       n_ = std::accumulate(extents_.begin(),
                            extents_.end(),
-                           1,
-                           std::multiplies<unsigned>());
+                           static_cast<size_t>(1),
+                           std::multiplies<size_t>());
       if(Padding)
         n_padded_ = n_ / extents_[NDim-1] * (extents_[NDim-1]/2 + 1);
 
       data_size_ = ( Padding ? 2*n_padded_ : n_ ) * sizeof(RealOrComplexType);
       data_transform_size_ = IsInplace ? 0 : n_ * sizeof(ComplexType);
+      // There are some additional restrictions when using 64-bit API of cufft
+      // see http://docs.nvidia.com/cuda/cufft/#unique_1972991535
+      if(data_size_ >= (1ull<<32) || data_transform_size_ >= (1ull<<32))
+        use64bit_ = true;
     }
 
     ~CuFFTImpl() {
@@ -203,36 +237,64 @@ namespace CuFFT {
     size_t getPlanSize() {
       size_t size1 = 0;
       size_t size2 = 0;
-      init_forward();
-      size1 = estimateAllocSize<FFTForward>(extents_,plan_);
-      init_backward();
-      size2 = estimateAllocSize<FFTBackward>(extents_,plan_);
-      CHECK_CUFFT( cufftDestroy(plan_) );
+      if(use64bit_)
+        size1 = estimateAllocSize64<FFTForward>(plan_,extents_);
+      else{
+        size1 = estimateAllocSize<FFTForward>(plan_,extents_);
+      }
+      CHECK_CUDA(cufftDestroy(plan_));
+      plan_=0;
+      if(use64bit_)
+        size2 = estimateAllocSize64<FFTBackward>(plan_,extents_);
+      else{
+        size2 = estimateAllocSize<FFTBackward>(plan_,extents_);
+      }
+      CHECK_CUDA(cufftDestroy(plan_));
+      plan_=0;
+
+      // check available GPU memory
+      size_t mem_free=0, mem_tot=0;
+      CHECK_CUDA( cudaMemGetInfo(&mem_free, &mem_tot) );
+      size_t wanted = std::max(size1,size2) + data_size_ + data_transform_size_;
+      if(mem_free<wanted) {
+        std::stringstream ss;
+        ss << mem_free << "<" << wanted << " (bytes)";
+        throw std::runtime_error("Not enough GPU memory available. "+ss.str());
+      }
       return std::max(size1,size2);
     }
 
     // --- next methods are benchmarked ---
 
     void malloc() {
-      if(IsInplace){
-        CHECK_CUDA(cudaMalloc(&data_, data_size_));
+      CHECK_CUDA(cudaMalloc(&data_, data_size_));
+      if(!data_) throw std::runtime_error("Could not allocate GPU input buffer.");
+
+      if(IsInplace) {
         data_transform_ = reinterpret_cast<ComplexType*>(data_);
       }else{
-        CHECK_CUDA(cudaMalloc(&data_, data_size_));
         CHECK_CUDA(cudaMalloc(&data_transform_, data_transform_size_));
+        if(!data_transform_) throw std::runtime_error("Could not allocate GPU buffer for outplace transform.");
       }
     }
 
     // create FFT plan handle
     void init_forward() {
-      makePlan<FFTForward>(plan_, extents_);
+      if(use64bit_)
+        makePlan64<FFTForward>(plan_, extents_);
+      else
+        makePlan<FFTForward>(plan_, extents_);
     }
 
     // recreates plan if needed
     void init_backward() {
       if(IsComplex==false){
-        CHECK_CUFFT(cufftDestroy(plan_));
-        makePlan<FFTBackward>(plan_, extents_);
+        CHECK_CUDA(cufftDestroy(plan_));
+        plan_=0;
+        if(use64bit_)
+          makePlan64<FFTBackward>(plan_, extents_);
+        else
+          makePlan<FFTBackward>(plan_, extents_);
       }
     }
 
@@ -246,8 +308,7 @@ namespace CuFFT {
 
     template<typename THostData>
     void upload(THostData* input) {
-      if(Padding) // real + inplace + ndim>1
-      {
+      if(Padding) { // real + inplace + ndim>1
         size_t w      = extents_[NDim-1] * sizeof(THostData);
         size_t h      = n_ * sizeof(THostData) / w;
         size_t pitch  = (extents_[NDim-1]/2+1) * sizeof(ComplexType);
@@ -259,8 +320,7 @@ namespace CuFFT {
 
     template<typename THostData>
     void download(THostData* output) {
-      if(Padding) // real + inplace + ndim>1
-      {
+      if(Padding) { // real + inplace + ndim>1
         size_t w      = extents_[NDim-1] * sizeof(THostData);
         size_t h      = n_ * sizeof(THostData) / w;
         size_t pitch  = (extents_[NDim-1]/2+1) * sizeof(ComplexType);
@@ -271,16 +331,14 @@ namespace CuFFT {
     }
 
     void destroy() {
-      if(data_) {
-        CHECK_CUDA( cudaFree(data_) );
-        data_=nullptr;
-        if(IsInplace==false && data_transform_) {
-          CHECK_CUDA( cudaFree(data_transform_) );
-          data_transform_ = nullptr;
-        }
+      CHECK_CUDA( cudaFree(data_) );
+      data_=nullptr;
+      if(IsInplace==false && data_transform_) {
+        CHECK_CUDA( cudaFree(data_transform_) );
+        data_transform_ = nullptr;
       }
       if(plan_) {
-        CHECK_CUFFT( cufftDestroy(plan_) );
+        CHECK_CUDA( cufftDestroy(plan_) );
         plan_=0;
       }
     }
