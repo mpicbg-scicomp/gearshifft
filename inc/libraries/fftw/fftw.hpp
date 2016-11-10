8d71041528e12a91c3e6c965580739c42e9e1f9e
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2016-11-10 19:39:16 +0100

    skip if more than 95% of total RAM will be used.

--- inc/libraries/fftw/fftw.hpp
+++ inc/libraries/fftw/fftw.hpp
@@ -450,7 +450,7 @@ namespace fftw {
         data_size_ = sizeof(value_type)*n_allocated_;
         data_transform_size_ = IsInplace ? 0 : n_ * sizeof(ComplexType);
         //size_t total_mem = getMemorySize();
-        size_t total_mem = 90*getMemorySize()/100; // keep some memory available, otherwise an out-of-memory killer is likely
+        size_t total_mem = 95*getMemorySize()/100; // keep some memory available, otherwise an out-of-memory killer becomes more likely
         if(total_mem < 2*(data_size_+data_transform_size_)) { // includes host input buffers
           std::stringstream ss;
           ss << total_mem << "<" << 2*(data_size_+data_transform_size_) << " (bytes)";
