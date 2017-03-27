70f14aaa28159da15bd87605e0ff151607c0116d
Peter Steinbach <steinbac@mpi-cbg.de>
2017-03-27 13:15:37 +0200

    added numactl to PBS submit scripts

--- results/P100-PCIE-16GB/clfft-2.12.2/hypnos.sh
+++ results/P100-PCIE-16GB/clfft-2.12.2/hypnos.sh
@@ -11,7 +11,8 @@
 
 source /opt/modules-3.2.6/Modules/3.2.6/init/bash
 module load cuda/8.0 gcc/5.3.0 boost/1.63.0 amd-app-sdk//3.0.130.136 googletest/1.8.0 clfft/2.12.2 
+module load numactl/2.0.7
 
 #export CUDA_VISIBLE_DEVICES=0 #P100
 
-/home/steinb95/development/gearshifft/build/gearshifft_clfft -d 0:0 -f /home/steinb95/development/gearshifft/config/extents_all_publication.conf -o /home/steinb95/development/gearshifft/results/P100-PCIE-16GB/clfft-2.12.2/clfft_gcc5.3.0_ubuntu14.04.5.csv
+numactl -m0 -N0 /home/steinb95/development/gearshifft/build/gearshifft_clfft -d 0:0 -f /home/steinb95/development/gearshifft/config/extents_all_publication.conf -o /home/steinb95/development/gearshifft/results/P100-PCIE-16GB/clfft-2.12.2/clfft_gcc5.3.0_ubuntu14.04.5.csv
