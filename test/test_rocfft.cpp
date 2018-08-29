ff8909a3ba681968f6f7604a59b11535580cc0b2
Peter Steinbach <steinbach@scionics.de>
2018-08-29 11:03:26 +0200

    test_rocfft finished, runs under ROCm 1.8.192-1 with rocfft 0.8.4

--- test/test_rocfft.cpp
+++ test/test_rocfft.cpp
@@ -8,20 +8,19 @@
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 
-BOOST_AUTO_TEST_CASE( FFT1DSingle, * boost::unit_test::tolerance(0.0001f) )
+BOOST_AUTO_TEST_CASE( FFT1DSmall, * boost::unit_test::tolerance(0.0001f) )
 {
   // rocFFT gpu compute
         // ========================================
 
         rocfft_setup();
 
-        std::size_t N = 32 << 10;
+        std::size_t N = 2 << 10; //should be small enough to not need a work_buffer
         std::size_t Nbytes = N * sizeof(float2);
 
         // Create HIP device buffer
         float2 *x;
         hipMalloc(&x, Nbytes);
-
         // Initialize data
         std::vector<float2> cx(N);
         for (std::size_t i = 0; i < N; i++)
@@ -32,7 +31,6 @@ BOOST_AUTO_TEST_CASE( FFT1DSingle, * boost::unit_test::tolerance(0.0001f) )
 
         //  Copy data to device
         hipMemcpy(x, cx.data(), Nbytes, hipMemcpyHostToDevice);
-
         // Create fwd rocFFT fwd
         rocfft_plan fwd = NULL;
         std::size_t length = N;
@@ -49,6 +47,7 @@ BOOST_AUTO_TEST_CASE( FFT1DSingle, * boost::unit_test::tolerance(0.0001f) )
                            rocfft_transform_type_complex_inverse,
                            rocfft_precision_single,
                            1, &length, 1, NULL);
+
 
         // Execute fwd
         rocfft_execute(fwd, (void**) &x, NULL, NULL);
@@ -91,3 +90,106 @@ BOOST_AUTO_TEST_CASE( FFT1DSingle, * boost::unit_test::tolerance(0.0001f) )
 
 }
 
+BOOST_AUTO_TEST_CASE( FFT1D, * boost::unit_test::tolerance(0.0001f) )
+{
+        // rocFFT gpu compute
+        // ========================================
+
+        rocfft_setup();
+
+        std::size_t N = 128 << 10; //needs a work_buffer
+        std::size_t Nbytes = N * sizeof(float2);
+
+        // Create HIP device buffer
+        float2 *x;
+        hipMalloc(&x, Nbytes);
+        // Initialize data
+        std::vector<float2> cx(N);
+        for (std::size_t i = 0; i < N; i++)
+        {
+                cx[i].x = 1;
+                cx[i].y = -1;
+        }
+
+        //  Copy data to device
+        hipMemcpy(x, cx.data(), Nbytes, hipMemcpyHostToDevice);
+        // Create fwd rocFFT fwd
+        rocfft_plan fwd = NULL;
+        std::size_t length = N;
+        rocfft_plan_create(&fwd,
+                           rocfft_placement_inplace,
+                           rocfft_transform_type_complex_forward,
+                           rocfft_precision_single,
+                           1, &length, 1, NULL);
+
+        // Create bwd rocFFT bwd
+        rocfft_plan bwd = NULL;
+        rocfft_plan_create(&bwd,
+                           rocfft_placement_inplace,
+                           rocfft_transform_type_complex_inverse,
+                           rocfft_precision_single,
+                           1, &length, 1, NULL);
+
+        // Setup work buffer
+        void *workBuffer = nullptr;
+        size_t workBufferSize_fwd = 0;
+        rocfft_plan_get_work_buffer_size(fwd, &workBufferSize_fwd);
+        size_t workBufferSize_bwd = 0;
+        rocfft_plan_get_work_buffer_size(bwd, &workBufferSize_bwd);
+
+        BOOST_REQUIRE_EQUAL(workBufferSize_bwd,workBufferSize_fwd);
+
+        // Setup exec info to pass work buffer to the library
+        rocfft_execution_info info = nullptr;
+        rocfft_execution_info_create(&info);
+
+        if(workBufferSize_fwd > 0)
+        {
+                hipMalloc(&workBuffer, workBufferSize_fwd);
+                rocfft_execution_info_set_work_buffer(info, workBuffer, workBufferSize_fwd);
+        }
+
+        // Execute fwd
+        rocfft_execute(fwd, (void**) &x, NULL, info);
+
+        // Wait for execution to finish
+        hipDeviceSynchronize();
+
+        // Destroy fwd
+        rocfft_plan_destroy(fwd);
+
+        // Execute bwd
+        rocfft_execute(bwd, (void**) &x, NULL, info);
+
+        // Wait for execution to finish
+        hipDeviceSynchronize();
+
+        // Destroy bwd
+        rocfft_plan_destroy(bwd);
+
+        if(workBuffer)
+  	     	hipFree(workBuffer);
+
+        rocfft_execution_info_destroy(info);
+
+        // Copy result back to host
+        std::vector<float2> y(N);
+        hipMemcpy(y.data(), x, Nbytes, hipMemcpyDeviceToHost);
+
+        // Print results
+        for (std::size_t i = 0; i < N; i++)
+        {
+                y[i].x *= 1.f/N;
+                y[i].y *= 1.f/N;
+
+                BOOST_TEST( cx[i].x == y[i].x );
+                BOOST_TEST( cx[i].y == y[i].y );
+        }
+
+        // Free device buffer
+        hipFree(x);
+
+        rocfft_cleanup();
+
+
+}
