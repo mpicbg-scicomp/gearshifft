0c38cd2b3edbc872e0adccbe34b76d8c8ce85278
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2018-09-02 21:08:32 +0200

    cmake distro finder also looks in /etc/os-release.

--- cmake/linux-distro-helper.cmake
+++ cmake/linux-distro-helper.cmake
@@ -7,14 +7,25 @@ macro(get_linux_distro_name)
   if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 
     find_program(LSB_RELEASE_EXEC lsb_release)
+    mark_as_advanced(LSB_RELEASE_EXEC)
 
     if(LSB_RELEASE_EXEC)
       execute_process(COMMAND ${LSB_RELEASE_EXEC} -is
         OUTPUT_VARIABLE GEARSHIFFT_LINUX_DISTRO
         OUTPUT_STRIP_TRAILING_WHITESPACE
         )
+    else()
+      find_path(OS_RELEASE
+        NAMES "os-release"
+        PATHS /etc
+        NO_DEFAULT_PATH)
+      if(OS_RELEASE)
+        execute_process(COMMAND "awk -F= '/^NAME/{print $2}' /etc/os-release"
+          OUTPUT_VARIABLE GEARSHIFFT_LINUX_DISTRO
+          OUTPUT_STRIP_TRAILING_WHITESPACE
+          )
+      endif()
     endif()
-    mark_as_advanced(LSB_RELEASE_EXEC)
   endif()
 
 endmacro()
