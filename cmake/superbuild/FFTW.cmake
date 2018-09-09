a5f6ec358a2331316bcb4711b60324faef8a2947
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2018-09-09 21:55:37 +0200

    Adds superbuild cmake (auto install of Boost, FFTW, clfft) (static linking).

---
+++ cmake/superbuild/FFTW.cmake
@@ -0,0 +1,77 @@
+#---------------------------------------------------------------------------
+# Get and build fftw (double+single), if it is not already installed in /gearshifft/ext
+
+set(GEARSHIFFT_EXT_FFTW_VERSION "3.3.8" CACHE STRING "FFTW version to be built.")
+set_property(CACHE GEARSHIFFT_EXT_FFTW_VERSION PROPERTY STRINGS "3.3.8")
+
+set(FFTW_ROOT ${GEARSHIFFT_EXT_DIR}/fftw CACHE PATH "'FFTW' root directory")
+
+find_path(FFTW_INCLUDE_DIR
+  NAMES fftw3.h
+  PATHS ${FFTW_ROOT}/include
+  NO_DEFAULT_PATH)
+
+find_library(FFTW_LIBRARY_DIR
+  NAMES fftw3
+  PATHS ${FFTW_ROOT}/lib
+  NO_DEFAULT_PATH)
+
+find_library(FFTWF_LIBRARY_DIR
+  NAMES fftw3f
+  PATHS ${FFTW_ROOT}/lib
+  NO_DEFAULT_PATH)
+
+if((NOT FFTW_INCLUDE_DIR) OR (NOT EXISTS ${FFTW_INCLUDE_DIR})
+    OR
+    (NOT FFTW_LIBRARY_DIR) OR (NOT EXISTS ${FFTW_LIBRARY_DIR})
+    OR
+    (NOT FFTWF_LIBRARY_DIR) OR (NOT EXISTS ${FFTWF_LIBRARY_DIR})
+    )
+
+  message("'fftw' and 'fftwf' library could not be found, so [make] will download and build it.")
+
+  # md5 hashes of tar.gz files
+  if( UNIX OR APPLE )
+    if(GEARSHIFFT_EXT_FFTW_VERSION MATCHES "3.3.8")
+      set(FFTW_hash "8aac833c943d8e90d51b697b27d4384d")
+    else()
+      set(FFTW_hash "")
+    endif()
+
+    set( FFTW_url "http://www.fftw.org/fftw-${GEARSHIFFT_EXT_FFTW_VERSION}.tar.gz")
+  else()
+    message(FATAL_ERROR "Windows is not supported yet.")
+  endif()
+
+  if(GEARSHIFFT_USE_STATIC_LIBS)
+    set(BUILD_LIBS "static")
+  else()
+    set(BUILD_LIBS "shared")
+  endif()
+
+  set(FFTW_BUILD_FLAGS --enable-openmp --enable-sse2 -q --enable-${BUILD_LIBS}=yes --with-gnu-ld  --enable-silent-rules --with-pic)
+  ExternalProject_Add(fftw
+    BUILD_IN_SOURCE 1
+    URL ${FFTW_url}
+    URL_MD5 ${FFTW_hash}
+    DOWNLOAD_DIR ${GEARSHIFFT_EXT_DIR}/downloads/
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ./configure --prefix=${FFTW_ROOT} ${FFTW_BUILD_FLAGS}
+    BUILD_COMMAND make -j 4
+    INSTALL_COMMAND make install
+    )
+  set(gearshifft_DEPENDENCIES fftw)
+
+  ExternalProject_Add(fftwf
+    BUILD_IN_SOURCE 1
+    URL ${FFTW_url}
+    URL_MD5 ${FFTW_hash}
+    DOWNLOAD_DIR ${GEARSHIFFT_EXT_DIR}/downloads/
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ./configure --prefix=${FFTW_ROOT} --enable-float ${FFTW_BUILD_FLAGS}
+    BUILD_COMMAND make -j 4
+    INSTALL_COMMAND make install
+    )
+  set(gearshifft_DEPENDENCIES fftwf)
+
+endif()
---
+++ cmake/superbuild/clfft.cmake
@@ -0,0 +1,72 @@
+## based on: https://github.com/statismo/statismo/tree/master/superbuild
+
+#---------------------------------------------------------------------------
+# Get and build clfft, if it is not already installed in /gearshifft/ext
+
+set(GEARSHIFFT_EXT_CLFFT_VERSION "v2.12.2" CACHE STRING "clFFT version to be built.")
+set_property(CACHE GEARSHIFFT_EXT_CLFFT_VERSION PROPERTY STRINGS "v2.12.2;develop")
+set(CLFFT_ROOT ${GEARSHIFFT_EXT_DIR}/clfft/ CACHE PATH "'clFFT' root directory")
+
+find_path(CLFFT_INCLUDE_DIR
+  NAMES clFFT.h
+  PATHS ${CLFFT_ROOT}
+  PATH_SUFFIXES include
+  NO_DEFAULT_PATH)
+
+find_library(CLFFT_LIBRARY_DIR
+  NAMES clFFT
+  PATHS ${CLFFT_ROOT}
+  PATH_SUFFIXES lib lib64 lib/import lib64/import
+  NO_DEFAULT_PATH)
+
+if((NOT CLFFT_INCLUDE_DIR) OR (NOT EXISTS ${CLFFT_INCLUDE_DIR})
+    OR
+    (NOT CLFFT_LIBRARY_DIR) OR (NOT EXISTS ${CLFFT_LIBRARY_DIR}))
+
+  message("'clfft' library could not be found, so [make] will download and build it (if OpenCL is available).")
+
+  find_package(OpenCL)
+
+  if(OpenCL_FOUND)
+    find_package(Git)
+    if(NOT GIT_FOUND)
+      message(ERROR "Cannot find git. git is required for Superbuild")
+    endif()
+
+    option( USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." ON)
+
+    set(git_protocol "git")
+    if(NOT USE_GIT_PROTOCOL)
+      set(git_protocol "http")
+    endif()
+
+    if(GEARSHIFFT_USE_STATIC_LIBS)
+      set(BUILD_SHARED_LIBS OFF)
+    else()
+      set(BUILD_SHARED_LIBS ON)
+    endif()
+
+    ExternalProject_Add(clfft
+      GIT_REPOSITORY ${git_protocol}://github.com/clMathLibraries/clFFT.git
+      GIT_TAG ${GEARSHIFFT_EXT_CLFFT_VERSION}
+      UPDATE_COMMAND ""
+      PATCH_COMMAND ""
+      SOURCE_DIR clfft
+      SOURCE_SUBDIR src
+      BINARY_DIR clfft-build
+      CMAKE_ARGS
+      -DBUILD_CALLBACK_CLIENT:BOOL=OFF
+      -DBUILD_CLIENT:BOOL=ON
+      -DBUILD_EXAMPLES:BOOL=OFF
+      -DBUILD_TEST:BOOL=OFF
+      -DBUILD64=ON
+      -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+      -DCMAKE_INSTALL_PREFIX:PATH=${CLFFT_ROOT}
+      -DCMAKE_C_COMPILER:STRING=${CMAKE_C_COMPILER}
+      -DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}
+      )
+    set(gearshifft_DEPENDENCIES clfft)
+
+  endif()
+endif()
