831fec571b57f082154783f9fbb04579e94a91f7
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2018-09-16 18:09:30 +0200

    Better in-source build protection.

--- cmake/safeguards.cmake
+++ cmake/safeguards.cmake
@@ -1,25 +1,16 @@
-# Provides safeguards against in-source builds and bad build types.
-#
-# Variables used::
-#
-#   PROJECT_SOURCE_DIR
-#   PROJECT_BINARY_DIR
-#   CMAKE_BUILD_TYPE
 
-if(NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE "Release")
-endif()
+# resolve symlinks before comparing the paths
+# https://stackoverflow.com/a/22636874
+get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
+get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
 
-if(${PROJECT_SOURCE_DIR} STREQUAL ${PROJECT_BINARY_DIR})
+# disallow in-source builds
+if("${srcdir}" STREQUAL "${bindir}")
   message(FATAL_ERROR "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there.")
 endif()
 
-string(TOLOWER "${CMAKE_BUILD_TYPE}" cmake_build_type_tolower)
-string(TOUPPER "${CMAKE_BUILD_TYPE}" cmake_build_type_toupper)
-
-if(NOT cmake_build_type_tolower STREQUAL "debug" AND
-    NOT cmake_build_type_tolower STREQUAL "release" AND
-    NOT cmake_build_type_tolower STREQUAL "minsizerel" AND
-    NOT cmake_build_type_tolower STREQUAL "relwithdebinfo")
-  message(FATAL_ERROR "Unknown build type \"${CMAKE_BUILD_TYPE}\". Allowed values are Debug, Release, RelWithDebInfo, and MinSizeRel (case-insensitive).")
+# set default build type
+if (NOT CMAKE_BUILD_TYPE)
+  set(CMAKE_BUILD_TYPE "RelWithDebInfo" CACHE STRING "Build type")
+  message(STATUS "No build type selected, default to RelWithDebInfo")
 endif()
