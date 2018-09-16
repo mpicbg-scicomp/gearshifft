478e0593e79600d527ba72ca8c54ab552d8f88bf
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2018-09-16 22:16:58 +0200

    Packaging for DEB, TGZ, RPM improved (using components).

--- cmake/linux-distro-helper.cmake
+++ cmake/linux-distro-helper.cmake
@@ -2,30 +2,26 @@
 set(GEARSHIFFT_LINUX_DISTRO OFF CACHE BOOL "Name of linux distribution.")
 mark_as_advanced(GEARSHIFFT_LINUX_DISTRO)
 
-macro(get_linux_distro_name)
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 
-  if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  find_program(HELPER_LSB_RELEASE_EXEC lsb_release)
+  mark_as_advanced(HELPER_LSB_RELEASE_EXEC)
 
-    find_program(LSB_RELEASE_EXEC lsb_release)
-    mark_as_advanced(LSB_RELEASE_EXEC)
-
-    if(LSB_RELEASE_EXEC)
-      execute_process(COMMAND ${LSB_RELEASE_EXEC} -is
+  if(HELPER_LSB_RELEASE_EXEC)
+    execute_process(COMMAND ${HELPER_LSB_RELEASE_EXEC} -is
+      OUTPUT_VARIABLE GEARSHIFFT_LINUX_DISTRO
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+  else()
+    find_path(OS_RELEASE
+      NAMES "os-release"
+      PATHS /etc
+      NO_DEFAULT_PATH)
+    if(OS_RELEASE)
+      execute_process(COMMAND awk -F= "/^NAME/{print $2}" /etc/os-release
         OUTPUT_VARIABLE GEARSHIFFT_LINUX_DISTRO
         OUTPUT_STRIP_TRAILING_WHITESPACE
         )
-    else()
-      find_path(OS_RELEASE
-        NAMES "os-release"
-        PATHS /etc
-        NO_DEFAULT_PATH)
-      if(OS_RELEASE)
-        execute_process(COMMAND awk -F= "/^NAME/{print $2}" /etc/os-release
-          OUTPUT_VARIABLE GEARSHIFFT_LINUX_DISTRO
-          OUTPUT_STRIP_TRAILING_WHITESPACE
-          )
-      endif()
     endif()
   endif()
-
-endmacro()
+endif()
