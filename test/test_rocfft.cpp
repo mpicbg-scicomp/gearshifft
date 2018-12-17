503e61bba75e83d7ccf610550ff08e69c48da026
Peter Steinbach <steinbach@scionics.de>
2018-12-17 16:37:31 +0100

    added test with standard types

--- test/test_rocfft.cpp
+++ test/test_rocfft.cpp
@@ -1,6 +1,8 @@
 #define BOOST_TEST_MODULE TestrocFFT
 #include <iostream>
 #include <vector>
+#include <complex>
+
 #include <cstdint>
 #include "hip/hip_runtime_api.h"
 #include "hip/hip_vector_types.h"
@@ -89,6 +91,7 @@ BOOST_AUTO_TEST_CASE( FFT1DSmall, * boost::unit_test::tolerance(0.0001f) )
 
 
 }
+
 
 BOOST_AUTO_TEST_CASE( FFT1D, * boost::unit_test::tolerance(0.0001f) )
 {
@@ -184,6 +187,88 @@ BOOST_AUTO_TEST_CASE( FFT1D, * boost::unit_test::tolerance(0.0001f) )
 
                 BOOST_TEST( cx[i].x == y[i].x );
                 BOOST_TEST( cx[i].y == y[i].y );
+        }
+
+        // Free device buffer
+        hipFree(x);
+
+        rocfft_cleanup();
+
+
+}
+
+BOOST_AUTO_TEST_CASE( FFT1DSmall_stdcomplex, * boost::unit_test::tolerance(0.0001f) )
+{
+  // rocFFT gpu compute
+        // ========================================
+
+        rocfft_setup();
+
+        std::size_t N = 2 << 10; //should be small enough to not need a work_buffer
+        std::size_t Nbytes = N * sizeof(std::complex<float>);
+
+        // Create HIP device buffer
+        std::complex<float> *x;
+        hipMalloc(&x, Nbytes);
+        // Initialize data
+        std::vector<std::complex<float>> cx(N);
+        for (std::size_t i = 0; i < N; i++)
+        {
+                cx[i].real( 1 );
+                cx[i].imag( -1);
+        }
+
+        //  Copy data to device
+        hipMemcpy(x, cx.data(), Nbytes, hipMemcpyHostToDevice);
+        // Create fwd rocFFT fwd
+        rocfft_plan fwd = NULL;
+        std::size_t length = N;
+        rocfft_plan_create(&fwd,
+                           rocfft_placement_inplace,
+                           rocfft_transform_type_complex_forward,
+                           rocfft_precision_single,
+                           1, &length, 1, NULL);
+
+        // Create bwd rocFFT bwd
+        rocfft_plan bwd = NULL;
+        rocfft_plan_create(&bwd,
+                           rocfft_placement_inplace,
+                           rocfft_transform_type_complex_inverse,
+                           rocfft_precision_single,
+                           1, &length, 1, NULL);
+
+
+        // Execute fwd
+        rocfft_execute(fwd, (void**) &x, NULL, NULL);
+
+        // Wait for execution to finish
+        hipDeviceSynchronize();
+
+        // Destroy fwd
+        rocfft_plan_destroy(fwd);
+
+        // Execute bwd
+        rocfft_execute(bwd, (void**) &x, NULL, NULL);
+
+        // Wait for execution to finish
+        hipDeviceSynchronize();
+
+        // Destroy bwd
+        rocfft_plan_destroy(bwd);
+
+
+        // Copy result back to host
+        std::vector<std::complex<float>> y(N);
+        hipMemcpy(y.data(), x, Nbytes, hipMemcpyDeviceToHost);
+
+        // Print results
+        for (std::size_t i = 0; i < N; i++)
+        {
+          y[i].real ( y[i].real() * 1.f/N);
+          y[i].imag ( y[i].imag() * 1.f/N);
+
+          BOOST_TEST( cx[i].real() == y[i].real() );
+          BOOST_TEST( cx[i].imag() == y[i].imag() );
         }
 
         // Free device buffer
