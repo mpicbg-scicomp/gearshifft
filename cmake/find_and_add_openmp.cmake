c2fb61eb6fd0719605f450d6f90c6c9c4ac3914c
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2019-01-13 15:44:32 +0100

    Workaround for OpenMP::OpenMP_CXX.

--- cmake/find_and_add_openmp.cmake
+++ cmake/find_and_add_openmp.cmake
@@ -8,18 +8,15 @@ macro(find_and_add_openmp TARGET)
   # OpenMP is missing on macOS llvm default, for example
   if(OpenMP_FOUND OR OPENMP_FOUND OR OpenMP_CXX_FOUND)
 
+    # target_link_libraries(${TARGET} INTERFACE OpenMP::OpenMP_CXX) # does not work on interface, so we also must use set_property
     # CMake 3.9 FindOpenMP allows correct linking with Clang in more cases
-    if(NOT TARGET OpenMP::OpenMP_CXX)
-      find_package(Threads REQUIRED)
-      add_library(OpenMP::OpenMP_CXX IMPORTED INTERFACE)
-      # Clang may need -fopenmp=libiomp5 instead, can't be detected here without CMake 3.9
-      set_property(TARGET OpenMP::OpenMP_CXX
-        PROPERTY INTERFACE_COMPILE_OPTIONS ${OpenMP_CXX_FLAGS})
-      # Only works if the same flag is passed to the linker; use CMake 3.9+ otherwise (Intel, AppleClang)
-      set_property(TARGET OpenMP::OpenMP_CXX
-        PROPERTY INTERFACE_LINK_LIBRARIES ${OpenMP_CXX_FLAGS} Threads::Threads)
-    endif()
-    target_link_libraries(${TARGET} INTERFACE OpenMP::OpenMP_CXX)
+    find_package(Threads REQUIRED)
+    # Clang may need -fopenmp=libiomp5 instead, can't be detected here without CMake 3.9
+    set_property(TARGET ${TARGET}
+      APPEND PROPERTY INTERFACE_COMPILE_OPTIONS ${OpenMP_CXX_FLAGS})
+    # Only works if the same flag is passed to the linker; use CMake 3.9+ otherwise (Intel, AppleClang)
+    set_property(TARGET ${TARGET}
+      APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${OpenMP_CXX_FLAGS} Threads::Threads)
   endif() # OpenMP_FOUND
 
 endmacro()
