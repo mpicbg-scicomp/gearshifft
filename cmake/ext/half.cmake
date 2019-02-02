6101cb6b74ef97b4b9621240af2e1dd22a7036e4
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2019-02-02 10:41:07 +0100

    Fixes half cmake ext path when superbuild mode is off.

--- cmake/ext/half.cmake
+++ cmake/ext/half.cmake
@@ -1,11 +1,17 @@
-cmake_minimum_required(VERSION 3.7)
+#---------------------------------------------------------------------------
+# Get and build half, if it is not already present
 
 set(GEARSHIFFT_HALF_VERSION "1.12.0" CACHE STRING "'half' version to be built.")
 set_property(CACHE GEARSHIFFT_HALF_VERSION PROPERTY STRINGS "1.12.0")
 
-set(half_INCLUDE_DIR ${GEARSHIFFT_SUPERBUILD_EXT_DIR}/half/src/ExtHalf/include
-  CACHE PATH "'half' include directory")
+if(NOT GEARSHIFFT_SUPERBUILD_EXT_DIR)
+  set(half_BASE_DIR ${GEARSHIFFT_ROOT}/ext/half)
+else()
+  set(half_BASE_DIR ${GEARSHIFFT_SUPERBUILD_EXT_DIR}/half)
+endif()
 
+set(half_INCLUDE_DIR ${half_BASE_DIR}/src/ExtHalf/include
+  CACHE PATH "'half' include directory")
 
 # for convenience setup a target
 add_library(half INTERFACE)
@@ -24,7 +30,7 @@ if((NOT _half_INCLUDE_DIR) OR (NOT EXISTS ${half_INCLUDE_DIR}))
 
   # half-code is a header-only library, so no build and install required
   ExternalProject_Add(ExtHalf
-    PREFIX ${GEARSHIFFT_SUPERBUILD_EXT_DIR}/half
+    PREFIX ${half_BASE_DIR}
     URL https://downloads.sourceforge.net/project/half/half/${GEARSHIFFT_HALF_VERSION}/half-${GEARSHIFFT_HALF_VERSION}.zip
     URL_MD5 86d023c0729abf3465bcd55665a39013
     INSTALL_COMMAND ""
