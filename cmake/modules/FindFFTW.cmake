a2710027408d35ebf8ec902eb18173c83b8637ea
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2019-03-15 22:22:09 +0100

    FFTWWrappers: Init FFTW threads define. Static libs. CLI options.
    - cmake: GEARSHIFFT_BACKEND_FFTW_THREADS -> GEARSHIFFT_INTERNAL_FFTWX_THREADED
    - FFTWWrappers/MKL does not implement parameters for rigor & timelimit
      - so do not show CLI options for that
    - Fixes and moves non-MKL libs in MKL cmake variable.
    - show messages only when not set to quiet.
    - Only restrict suffix for static libs (also in FindFFTW.cmake)

--- cmake/modules/FindFFTW.cmake
+++ cmake/modules/FindFFTW.cmake
@@ -2,16 +2,16 @@
 #
 # Usage:
 #   find_package(FFTW [REQUIRED] [QUIET] [COMPONENTS ...])
-#     
+#
 # It sets the following variables:
 #   FFTW_FOUND          ... true if fftw is found on the system
 #   FFTW_LIBRARIES      ... list of library identifiers that were found (will be filled with serial/openmp/pthreads enabled file names if present, only stubs will be filled in not full paths)
 #   FFTW_INCLUDES       ... fftw include directory
-#   FFTW_SERIAL_LIBS	... list of full path(s) to serial fftw library(ies)
-#   FFTW_THREADS_LIBS	... list of full path(s) to pthreads fftw library(ies)
-#   FFTW_OPENMP_LIBS	... list of full path(s) to openmp fftw library(ies)
-#   FFTW_INCLUDE_DIR	... fftw include directory
-#   FFTW_LIBRARY_DIR	... fftw library directory
+#   FFTW_SERIAL_LIBS  ... list of full path(s) to serial fftw library(ies)
+#   FFTW_THREADS_LIBS ... list of full path(s) to pthreads fftw library(ies)
+#   FFTW_OPENMP_LIBS  ... list of full path(s) to openmp fftw library(ies)
+#   FFTW_INCLUDE_DIR  ... fftw include directory
+#   FFTW_LIBRARY_DIR  ... fftw library directory
 #
 # The following variables will be checked by the function
 #   FFTW_USE_STATIC_LIBS    ... if true, only static libraries are found
@@ -60,12 +60,12 @@ endforeach()
 
 #If environment variable FFTWDIR is specified, it has same effect as FFTW_ROOT
 if( NOT FFTW_ROOT AND (DEFINED ENV{FFTWDIR} OR DEFINED ENV{FFTW_ROOT}))
-  if(EXISTS "$ENV{FFTWDIR}/")                                          
-    set( FFTW_ROOT $ENV{FFTWDIR} )                                     
-  endif()                                                              
-  if( EXISTS "$ENV{FFTW_ROOT}/" )                                      
-    set( FFTW_ROOT $ENV{FFTW_ROOT} )                                   
-  endif()                                                              
+  if(EXISTS "$ENV{FFTWDIR}/")
+    set( FFTW_ROOT $ENV{FFTWDIR} )
+  endif()
+  if( EXISTS "$ENV{FFTW_ROOT}/" )
+    set( FFTW_ROOT $ENV{FFTW_ROOT} )
+  endif()
 endif()
 
 # Check if we can use PkgConfig
@@ -79,17 +79,15 @@ endif()
 #Check whether to search static or dynamic libs
 set( CMAKE_FIND_LIBRARY_SUFFIXES_SAV ${CMAKE_FIND_LIBRARY_SUFFIXES} )
 
-if( ${FFTW_USE_STATIC_LIBS} )
+if( FFTW_USE_STATIC_LIBS ) # find static libs only
   set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX} )
-else()
-  set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_SHARED_LIBRARY_SUFFIX} )
 endif()
 
 #initialize library variables
 foreach(_LIBSTUB IN LISTS FFTW_LIBRARY_STUBS)
-  
+
   if( FFTW_ROOT )
-    
+
     find_library(
       FFTW_SERIAL_STUBLIB
       NAMES ${_LIBSTUB} lib${_LIBSTUB}
@@ -113,7 +111,7 @@ foreach(_LIBSTUB IN LISTS FFTW_LIBRARY_STUBS)
       PATH_SUFFIXES "lib" "lib64"
       NO_DEFAULT_PATH
       )
-    
+
   else( FFTW_ROOT )
     find_library(
       FFTW_SERIAL_STUBLIB
@@ -133,9 +131,10 @@ foreach(_LIBSTUB IN LISTS FFTW_LIBRARY_STUBS)
       NAMES ${_LIBSTUB}_threads lib${_LIBSTUB}_threads
       PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
       )
-    
+
   endif( FFTW_ROOT )
 
+  # manually build shared libs from static libs (if only static libs were found)
   if(EXISTS ${FFTW_SERIAL_STUBLIB})
 
     if( NOT CMAKE_FIND_LIBRARY_SUFFIXES MATCHES ${CMAKE_STATIC_LIBRARY_SUFFIX} )
@@ -168,7 +167,7 @@ foreach(_LIBSTUB IN LISTS FFTW_LIBRARY_STUBS)
 
     list(APPEND FFTW_OPENMP_LIBS_ABSPATH ${FFTW_OPENMP_STUBLIB})
     list(APPEND FFTW_OPENMP_LIBS ${_LIBSTUB}_omp)
-    
+
     list(APPEND FFTW_LIBRARIES ${_LIBSTUB}_omp)
 
   else()
@@ -187,7 +186,7 @@ foreach(_LIBSTUB IN LISTS FFTW_LIBRARY_STUBS)
 
     list(APPEND FFTW_THREADS_LIBS_ABSPATH ${FFTW_THREADS_STUBLIB})
     list(APPEND FFTW_THREADS_LIBS ${_LIBSTUB}_threads)
-    
+
     list(APPEND FFTW_LIBRARIES ${_LIBSTUB}_threads)
   else()
     if(NOT FFTW_FIND_QUIETLY)
@@ -198,12 +197,12 @@ foreach(_LIBSTUB IN LISTS FFTW_LIBRARY_STUBS)
   unset(FFTW_SERIAL_STUBLIB CACHE)
   unset(FFTW_THREADS_STUBLIB CACHE)
   unset(FFTW_OPENMP_STUBLIB CACHE)
-  
+
 endforeach()
 
 #find includes
 if( FFTW_ROOT )
-  
+
   find_path(
     FFTW_INCLUDES
     NAMES "fftw3.h"
@@ -247,7 +246,7 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FFTW DEFAULT_MSG
   FFTW_INCLUDES
   FFTW_LIBRARIES
-#  HANDLE_COMPONENTS
+  #  HANDLE_COMPONENTS
   )
 
 if(NOT FFTW_FIND_QUIETLY)
@@ -255,7 +254,7 @@ if(NOT FFTW_FIND_QUIETLY)
   message("++ FFTW_INCLUDES    : ${FFTW_INCLUDES}")
   message("++ FFTW_LIBRARIES   : ${FFTW_LIBRARIES}")
   message("++ FFTW_SERIAL_LIBS : ${FFTW_SERIAL_LIBS_ABSPATH}")
-  message("++ FFTW_THREADS_LIBS: ${FFTW_THREADS_LIBS_ABSPATH}") 
+  message("++ FFTW_THREADS_LIBS: ${FFTW_THREADS_LIBS_ABSPATH}")
   message("++ FFTW_OPENMP_LIBS : ${FFTW_OPENMP_LIBS_ABSPATH}")
   message("++ FFTW_INCLUDE_DIR : ${FFTW_INCLUDE_DIR}")
   message("++ FFTW_LIBRARY_DIR : ${FFTW_LIBRARY_DIR}")
@@ -270,4 +269,3 @@ mark_as_advanced(
   FFTW_INCLUDE_DIR
   FFTW_LIBRARY_DIR
   )
-
